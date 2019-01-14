6e0d95feb6c19c6676cf37325a30fc9aa8bf8408
Yunkun Guo <yunkun.dev@gmail.com>
2019-01-14 00:40:08 -0800

    color-game

---
+++ Color-Game/colorGame.js
@@ -0,0 +1,32 @@
+var colors = [
+	"rgb(255, 0, 0)",
+	"rgb(255, 255, 0)",
+	"rgb(0, 255, 0)",
+	"rgb(0, 255, 255)",
+	"rgb(0, 0, 255)",
+	"rgb(255, 0, 255)"
+]
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+	squares[i].style.backgroundColor = colors[i];
+}
+
+for(var i = 0; i < squares.length; i++){
+	squares[i].addEventListener("click", function(){
+		var clickedColor = this.style.backgroundColor; //string
+		if(clickedColor === pickedColor){
+			messageDisplay.textContent = "Correct!";
+		}
+		else{
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again";
+		}
+	})
+}
